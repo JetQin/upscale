58d8c6a096885b2bcda77ee639114f3ae9a2a57d
jet <jet.qin@pulseratech.com>
2019-06-23 22:58:57 +0800

    fix bugs

---
+++ src/test/groovy/io/github/jetqin/upscale/spec/LibraryTest.groovy
@@ -0,0 +1,18 @@
+package io.github.jetqin.upscale.spec
+
+import spock.lang.Specification
+
+class LibraryTest extends Specification {
+
+    def "test sum method"() {
+        given:
+        int left = 2
+        int right = 2
+
+        when:
+        int result = left + right
+
+        then:
+        result == 4
+    }
+}
--- src/test/groovy/io/github/jetqin/upscale/spec/PersonListSpec.groovy
+++ src/test/groovy/io/github/jetqin/upscale/spec/PersonListSpec.groovy
@@ -19,4 +19,16 @@ class PersonListSpec extends Specification{
         person.getName() == "Bruce"
 
     }
+
+    @Test
+    def "Should be able to remove from list"() {
+        given:
+        def list = [1, 2, 3, 4]
+
+        when:
+        list.remove(0)
+
+        then:
+        list == [2, 3, 4]
+    }
 }
